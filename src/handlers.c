1317a7c33baecbca9b72cbc34cc864e4c563a639
Guilherme Gomes <gui@guibgomes.com>
2023-02-01 09:57:30 +0100

    Working with handlers

--- src/handlers.c
+++ src/handlers.c
@@ -8,8 +8,8 @@ void ft_handle3(t_stack **lst)
     t_stack *tmp = *lst;
     if (tmp->index == 1 && tmp->next->index == 0)
     {
-        tmp = tmp->next;
-        tmp->next = *lst;
+        tmp->next = tmp;
+        tmp = (*lst)->next;
         lst = &tmp;
     }
 }
