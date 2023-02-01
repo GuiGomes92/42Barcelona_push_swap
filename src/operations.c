2a963bacaec68c8910320a2219c7dcb212a4d8ba
Guilherme Gomes <gui@guibgomes.com>
2023-02-01 12:05:41 +0100

    Added operations file and changed makefile to compile it.

---
+++ src/operations.c
@@ -0,0 +1,12 @@
+// TODO
+//  sa - swap top two in stack A.
+//  sb - swap top two in stack B.
+//  ss - run both above at the same time.
+//  ra - Top number goes to bottom of stack A.
+//  rb - Top number goes to bottom of stack B.
+//  rr - run both above at the same time.
+//  rra - Bottom number goes to top of stack A.
+//  rrb - Bottom number goes to top of stack B.
+//  rrr - run both above at the same time.
+//  pa - Send top of B to top of A.
+//  pb - - Send top of A to top of B.
